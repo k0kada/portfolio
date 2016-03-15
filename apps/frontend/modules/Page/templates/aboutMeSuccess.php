74a4cdaf378a276ef52d5ea583f6a5f6dcde7171
Koh Okada <koh.okada@gmail.com>
2016-03-15 20:13:00 +0000

    fix aboutMe

--- apps/frontend/modules/Page/templates/aboutMeSuccess.php
+++ apps/frontend/modules/Page/templates/aboutMeSuccess.php
@@ -16,58 +16,44 @@
 
   <div id="timeline">
     <ul id="dates">
-      <li><a href="#1900">1900</a></li>
-      <li><a href="#1930">1930</a></li>
-      <li><a href="#1944">1944</a></li>
-      <li><a href="#1950">1950</a></li>
-      <li><a href="#1971">1971</a></li>
-      <li><a href="#1977">1977</a></li>
-      <li><a href="#1989">1989</a></li>
-      <li><a href="#1999">1999</a></li>
-      <li><a href="#2001">2001</a></li>
+      <li><a href="#2007">2007</a></li>
       <li><a href="#2011">2011</a></li>
+      <li><a href="#2015">2013</a></li>
+      <li><a href="#2015">2015</a></li>
+      <li><a href="#2015">2015</a></li>
     </ul>
     <ul id="issues">
-      <li id="1900">
-        <h1>1900</h1>
-        <p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.</p>
-      </li>
-      <li id="1930">
-        <h1>1930</h1>
-        <p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.</p>
-      </li>
-      <li id="1944">
-        <h1>1944</h1>
-        <p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.</p>
-      </li>
-      <li id="1950">
-        <h1>1950</h1>
-        <p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.</p>
-      </li>
-      <li id="1971">
-        <h1>1971</h1>
-        <p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.</p>
-      </li>
-      <li id="1977">
-        <h1>1977</h1>
-        <p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.</p>
-      </li>
-      <li id="1989">
-        <h1>1989</h1>
-        <p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.</p>
-      </li>
-      <li id="1999">
-        <h1>1999</h1>
-        <p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.</p>
-      </li>
-      <li id="2001">
-        <h1>2001</h1>
-        <p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.</p>
+      <li id="2007">
+        <h1>2007</h1>
+        <h4>広島国際大学 入学</h4>
+        <p>心理学を専攻し、対話におけるコミュニケーションについて研究しました。</p>
+        <p>また、サークルでは子どもを対象としたボランティア部で活動していました。</p>
       </li>
       <li id="2011">
         <h1>2011</h1>
+        <h4>立教大学 大学院 入学</h4>
+        <p>社会学を専攻し、集団におけるコミュニケーションについて研究しました。</p>
+        <p>特にソーシャルメディアの観点から、『ナラティブ』というものをテーマにしていました。</p>
+      </li>
+      <li id="2013">
+        <h1>2013</h1>
+        <h4>TINAMI株式会社 入社</h4>
+        <?= link_to('http://www.tinami.com/', 'http://www.tinami.com/') ?>
+        <p>
+            イラスト投稿コミュニティサイトであるTINAMIに企画・営業として入社し、<br>
+            主に自社で行うイラストコンテストの企画や、広告出稿などの営業を行っていました。
+        </p>
+      </li>
+      <li id="2015">
+        <h1>2015</h1>
+        <h4>株式会社ごちぽん 入社</h4>
+        <?= link_to('http://gcpn.jp/', 'http://gcpn.jp/') ?>
+
+
         <p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.</p>
       </li>
+      <li></li>
+      <li></li>
     </ul>
     <div id="grad_left"></div>
     <div id="grad_right"></div>
