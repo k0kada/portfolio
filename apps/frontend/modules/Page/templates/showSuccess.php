86edf9d7bb1201d5197a53da2de298991145a721
Koh Okada <koh.okada@gmail.com>
2016-03-23 02:13:47 +0000

    topページに使用言語など追加

--- apps/frontend/modules/Page/templates/showSuccess.php
+++ apps/frontend/modules/Page/templates/showSuccess.php
@@ -3,3 +3,20 @@
   <p><?= nl2br($page->getRawValue()->getBody()) ?></p>
   <div id="author"><?= $page->getAuthor() ?></div>
 </div>
+
+<div id="block">
+  <table class="spec">
+    <tbody>
+      <tr>
+        <th class="arrow_box">言語</div></th>
+        <td>PHP<br>Java<br>JavaScript<br>HTML<br>CSS<br>MySQL</td>
+      </tr>
+      <tr>
+        <th>GitHub</th>
+        <td>
+          <?= link_to("https://github.com/k0kada", "https://github.com/k0kada") ?>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
