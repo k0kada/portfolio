42c729518faabea18421ac6e57d662177d9b4068
Koh Okada <koh.okada@gmail.com>
2016-03-31 13:36:19 +0000

    delete about

--- apps/frontend/templates/_Menu.php
+++ apps/frontend/templates/_Menu.php
@@ -1,6 +1,5 @@
 <ul>
   <li><?= link_to('トップ', 'homepage') ?></li>
-  <li><?= link_to('自己紹介', 'about_me') ?></li>
   <li><?= link_to('プロダクト', 'products') ?></li>
   <li><?= link_to('お知らせ', 'news_list') ?></li>
   <li><?= link_to('お問い合わせ', 'inquiry_new') ?></li>
