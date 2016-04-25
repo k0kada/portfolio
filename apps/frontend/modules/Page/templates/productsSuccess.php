8530a863789d661271083a1c8074543f67990a18
Koh Okada <koh.okada@gmail.com>
2016-04-25 08:37:22 +0000

    fix

--- apps/frontend/modules/Page/templates/productsSuccess.php
+++ apps/frontend/modules/Page/templates/productsSuccess.php
@@ -35,12 +35,12 @@ $(function(){
   </div>
 
   <div id="slider1">
-    <div><img src="/images/jumpman/menu.png" title="menu"></div>
-    <div><img src="/images/jumpman/stage1.png" title="stage1"></div>
-    <div><img src="/images/jumpman/stage2.png" title="stage2"></div>
-    <div><img src="/images/jumpman/stage3.png" title="stage3"></div>
-    <div><img src="/images/jumpman/stage4.png" title="stage4"></div>
-    <div><img src="/images/jumpman/dialog.png" title="確認ダイアログ"></div>
+    <div><img src="/sf14/web/images/jumpman/menu.png" title="menu"></div>
+    <div><img src="/sf14/web/images/jumpman/stage1.png" title="stage1"></div>
+    <div><img src="/sf14/web/images/jumpman/stage2.png" title="stage2"></div>
+    <div><img src="/sf14/web/images/jumpman/stage3.png" title="stage3"></div>
+    <div><img src="/sf14/web/images/jumpman/stage4.png" title="stage4"></div>
+    <div><img src="/sf14/web/images/jumpman/dialog.png" title="確認ダイアログ"></div>
   </div>
 
   <table class="spec">
@@ -82,9 +82,6 @@ $(function(){
     今回初めての試みだったのですが、PHPやApacheの導入をはじめ、
     前職で使っていたフレームワークであるSymfonyの導入まで行うことができ、
     業務ではあまり触れなかったサーバー設定などの理解を深めることができました。<br>
-    このサイトの特徴として、『お知らせ』の管理はfixtures.ymlを使いDBに保存、追加、削除などを
-    簡単にできるように設計しました。今後はcsvでのDB更新を行ったり、admin機能をつけるなどして
-    より簡単に情報の更新をできるようにと考えています。
 
   </div>
 
